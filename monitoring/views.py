c083b3e787e1f67951908853e7991a6fff5d19e6
LianZS <2214057556@qq.com>
2019-10-01 21:43:03 +0800

    任务传递

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .monitoring_goods import PinDuoDuo
+
 
 # Create your views here.
+@csrf_exempt
+def monitoring_pinduoduo_goods_by_url(request):
+    """
+    监控拼多多某个商品链接
+    方法：POST，参数
+    url：监控链接
+    monitoring_state:是否长期监控，1时0不是
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        url = request.POST.get("url")  # 监控链接
+        monitoring_state = request.POST.get("monitoring_state")  # 是否长期监控
+        pin_duo_duo =PinDuoDuo()
+        pin_duo_duo.get_goods_sold_by_url.delay(pin_duo_duo,url)
+    return HttpResponse("ok")
