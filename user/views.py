72db50ad47220fd4d0260781eeb37fe34dedf0d9
LianZS <2214057556@qq.com>
2019-10-04 10:39:38 +0800

    登陆表单

--- user/views.py
+++ user/views.py
@@ -1,15 +1,60 @@
 from django.shortcuts import render, HttpResponse
-from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
+from .forms import LoginForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 @csrf_exempt
-def login(request):
+def login_view(request):
+    """
+    登陆界面
+    :param request:
+    :return:
+    """
     if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            return HttpResponse("ok")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+        return HttpResponse("ok")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+
+def loginout_view(request):
+    """
+
+    :param request:
+    :return:
+    """
+    if request.user.is_authenticated:
+        loginout_view(request)
+        return HttpResponse("loginout success")
+    return HttpResponse("账号未登陆")
+
+
+@csrf_exempt
+def registered_view(request):
+    """
+    注册
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+        return HttpResponse("ok")
     else:
         form = LoginForm()
     return render(request, "login.html", {"form": form})
