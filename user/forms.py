2ba7c805435e38f251e9f182da6dec2f9fd9b677
LianZS <2214057556@qq.com>
2019-10-04 14:42:25 +0800

    修改placeholder属性

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import UserProfile
 
 
 class LoginForm(forms.ModelForm):
@@ -9,10 +8,10 @@ class LoginForm(forms.ModelForm):
         fields = ['username', 'password']
         widgets = {
             "username": forms.TextInput(
-                attrs={"class": "text", "style": "background-color: transparent", "value": "账号",
+                attrs={"class": "text", "style": "background-color: transparent", "placeholder": "账号",
                        "onfocus": "this.value=''"}),
             "password": forms.PasswordInput(
-                attrs={"class": "password", "style": "background-color: transparent", "value": "密码",
+                attrs={"class": "password", "style": "background-color: transparent", "placeholder": "密码",
                        "onfocus": "this.value=''"}),
         }
         labels = {
@@ -27,22 +26,19 @@ class LoginForm(forms.ModelForm):
 
 
 class RegisterForm(forms.ModelForm):
-
-
     class Meta:
         model = User
 
         fields = ['email', 'username', 'password']
         widgets = {
             "username": forms.TextInput(
-                attrs={"class": "text", "style": "background-color: transparent", "value": "账号",
+                attrs={"class": "text", "style": "background-color: transparent", "placeholder": "账号",
                        "onfocus": "this.value=''"}),
             "password": forms.PasswordInput(
-                attrs={"class": "password", "style": "background-color: transparent", "value": "密码",
+                attrs={"class": "password", "style": "background-color: transparent", "placeholder": "密码",
                        "onfocus": "this.value=''"}),
             "email": forms.EmailInput(
-                attrs={"class": "email", "style": "background-color: transparent", "value": "邮箱",
-                       "onfocus": "this.value=''"}),
+                attrs={"class": "email", "style": "background-color: transparent", "placeholder": "邮箱"}),
 
         }
         labels = {
