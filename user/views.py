4ff25fa943bd1b5700405fad75272a1d0afe0443
LianZS <2214057556@qq.com>
2019-10-04 14:38:57 +0800

    邮件发送功能

--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
+import random
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
-from .forms import LoginForm,RegisterForm
 from django.contrib.auth.models import User
+from django.core.cache import cache
+from .send_email import send_email
+from .forms import LoginForm, RegisterForm
 
 
 # Create your views here.
@@ -34,7 +38,7 @@ def loginout_view(request):
     :return:
     """
     if request.user.is_authenticated:
-        loginout_view(request)
+        logout(request)
         return HttpResponse("loginout success")
     return HttpResponse("账号未登陆")
 
@@ -58,3 +62,24 @@ def registered_view(request):
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
+
+
+def send_email_view(request):
+    if request.method == "GET":
+        verification_code = ''  # 验证码
+        for i in range(5):
+            verification_code += str(random.randint(0, 9))
+        email_address = request.GET['email']
+        subject = verification_code + "是你的验证码"
+        from_emial = 'publiccomany<publiccomany@163.com>'
+        to_email = email_address
+
+        message = "这事你的验证码:" + verification_code
+        html_content = "<html><p>确认你的邮件地址 在创建账号之前，你需要完成一个简单的步骤。让我们确保这是正确的邮件地址" \
+                       " — 请确认这是用于你的新账号的正确地址。 请输入此验证码以开始使用 ：</p>" \
+                       "<strong style='color:blue;font-size:30px'>{code}</strong> " \
+                       "<p>验证码两小时后过期。 谢谢！</p></html>".format(code=verification_code)
+        send_email.delay(EmailMultiAlternatives, from_emial, to_email, subject, message, html_content,
+                         verification_code, cache)
+
+    return HttpResponse("success")
