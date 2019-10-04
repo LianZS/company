10a162ba889c5f20625701690c5101150d05050e
LianZS <2214057556@qq.com>
2019-10-04 11:52:53 +0800

    扩展user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    idcard = models.BigIntegerField(db_column="idcard", verbose_name="身份证")
+    photo = models.ImageField(upload_to="user", null=True)  # 头像
