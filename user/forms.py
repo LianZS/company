742d2b23c1a2300c7bc6c9248b0117cbc8d64963
LianZS <2214057556@qq.com>
2019-10-04 11:52:40 +0800

    注册表单

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 class LoginForm(forms.ModelForm):
@@ -18,17 +19,41 @@ class LoginForm(forms.ModelForm):
             "username": '',
             "password": ''
         }
-        # error_messages = {
-        #     "username": {
-        #         "max_length": "This writer's name is too long",
-        #
-        #     },
-        #     "password": {
-        #         "max_length": "This writer's name is too long",
-        #
-        #     }
-        # }
+
         help_texts = {
             "username": '',
             "password": ''
+        }
+
+
+class RegisterForm(forms.ModelForm):
+
+
+    class Meta:
+        model = User
+
+        fields = ['email', 'username', 'password']
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"class": "text", "style": "background-color: transparent", "value": "账号",
+                       "onfocus": "this.value=''"}),
+            "password": forms.PasswordInput(
+                attrs={"class": "password", "style": "background-color: transparent", "value": "密码",
+                       "onfocus": "this.value=''"}),
+            "email": forms.EmailInput(
+                attrs={"class": "email", "style": "background-color: transparent", "value": "邮箱",
+                       "onfocus": "this.value=''"}),
+
+        }
+        labels = {
+            "username": '',
+            "password": '',
+            "email": '',
+
+        }
+
+        help_texts = {
+            "username": '',
+            "password": '',
+            "email": ''
         }
