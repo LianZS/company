8be28c342f79ba720400f65fb4b25f6f57f2d376
LianZS <2214057556@qq.com>
2019-10-04 14:38:36 +0800

    发送邮件路由

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
-from .views import login_view
+from .views import *
+
 urlpatterns = [
- path("login",login_view),
+    path("login", login_view),
+    path("register", registered_view),
+    path("forget", loginout_view),
+    path("sendmail", send_email_view),
+
 ]
