1845a5f6573f5c8c0b6ca43dd897563db41069aa
LianZS <2214057556@qq.com>
2019-10-05 20:21:49 +0800

    申请职位路由

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from .views import *
+from .manager_view import *
 
 urlpatterns = [
     path("login", login_view),
+    path("verification", validation_user),
+
     path("register", registered_view),
     path("forget", loginout_view),
-    path("sendmail", send_email_view),
+    path("sendVerificationCode", send_verification_view),
+    path("modify", modify_password),
+    path("manager/", include([
+        path('', manager_veiw),
+        path('recruitment', manager_recruitment),
 
+    ])),
+    path('recruitmentpage/<slug:uid>', recruitmentpage),
+    path('apply/<slug:uid>', apply_view),
 ]
