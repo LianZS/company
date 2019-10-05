6378c6802ea7f6d3952c0bc418ddfbe0e0daa39a
LianZS <2214057556@qq.com>
2019-10-05 16:35:11 +0800

    发送验证码

--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 import random
 from django.core.mail import EmailMultiAlternatives
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.core.cache import cache
 from .send_email import send_email
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ModifyForm
 
 
 # Create your views here.
@@ -21,14 +22,32 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-
         if user is not None:
             login(request, user)
+            return HttpResponseRedirect("/user/manager")
+        else:
+            status = "error"
 
-        return HttpResponse("ok")
     else:
         form = LoginForm()
-    return render(request, "login.html", {"form": form})
+
+        return render(request, "login.html", {"form": form})
+
+
+@csrf_exempt
+def validation_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect("/user/manager")
+        else:
+            status = "error"
+    else:
+        status = 'error'
+    return JsonResponse({"status": status})
 
 
 def loginout_view(request):
@@ -46,30 +65,79 @@ def loginout_view(request):
 @csrf_exempt
 def registered_view(request):
     """
-    注册
+    注册界面
     :param request:
     :return:
     """
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        User.objects.create_user(username=username, password=password)
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
+        email = request.POST['email']
+        verification_code = request.POST['verification']  # 验证码
+        right_verification_code = cache.get(email)
+        if verification_code == right_verification_code:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/user/manager')
+        else:
+            return HttpResponse('验证码有误')
+
+    else:
+        form = RegisterForm()
+        return render(request, "register.html", {"form": form})
+
+
+def manager_veiw(request):
+    """
+    管理界面
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            username = request.user.username
+            company = "品联优"
+            return render(request, 'manager.html', locals())
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def modify_password(request):
+    """
+    修改密码
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        email = request.POST['email']
+        verification_code = request.POST['verification']  # 验证码
+        right_verification_code = cache.get(email)
+        if verification_code == right_verification_code:
+            password = request.POST['password']
+
+            user = User.objects.get(email=email)
+            user.set_password(password)
+            user.save()
 
         return HttpResponse("ok")
     else:
-        form = RegisterForm()
-    return render(request, "register.html", {"form": form})
+        form = ModifyForm()
+    return render(request, "modify.html", {"form": form})
 
 
-def send_email_view(request):
-    if request.method == "GET":
+@csrf_exempt
+def send_verification_view(request):
+    """
+    发送验证码邮件
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
         verification_code = ''  # 验证码
         for i in range(5):
             verification_code += str(random.randint(0, 9))
-        email_address = request.GET['email']
+        email_address = request.POST['email']
         subject = verification_code + "是你的验证码"
         from_emial = 'publiccomany<publiccomany@163.com>'
         to_email = email_address
@@ -83,3 +151,4 @@ def send_email_view(request):
                          verification_code, cache)
 
     return HttpResponse("success")
+
